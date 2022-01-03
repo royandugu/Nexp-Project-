f4541363c08032f3d3ecd14095d88631ef3caf2d
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-01-03 17:20:56 +0545

    Created model Customer

--- eCommerce/authentication/models.py
+++ eCommerce/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    customer = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='images', null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.customer.username
