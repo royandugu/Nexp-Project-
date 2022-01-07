74d9fbe3cf8e3e3e8eb4b3971a704ebf77757d4d
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-01-07 20:56:37 +0545

    Authentication model of user completed

--- eCommerce/authentication/urls.py
+++ eCommerce/authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from authentication.views import login_request, login_view
+from authentication.views import login_request, login_view, logout_user, register, register_view
 
 urlpatterns = [
     path('login', login_view, name='login'),
     path('loggin_in', login_request, name='login_request'),
+    path('register', register_view, name='register'),
+    path('registering_in', register, name='register_request'),
+    path('logout', logout_user, name='logout'),
 ]
