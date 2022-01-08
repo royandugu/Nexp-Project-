f60460914c8bd64f37807fc302f0f320dadba31a
SarthakPaneru <sarthak.paneru007@gmail.com>
2022-01-08 12:33:03 +0545

    All auth installation requirements

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -39,7 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication',
     'home',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 ]
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
