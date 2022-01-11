3e0c5f7c33ef9ffbbf01ffe98a2b37ba581c5da5
myGithub123# <royandugu99009@gmail.com>
2022-01-11 22:29:22 +0545

    Product card addition

--- eCommerce/static/js/product.js
+++ eCommerce/static/js/product.js
@@ -8,14 +8,27 @@ for(let i=0;i<titles.length;i++){
 }
 //Top sellers
 const topSellers=document.querySelector(".sellerOptions");
-for(let i=0;i<6;i++){
-    let circle=document.createElement("div");
-    circle.setAttribute("class","sellers");
-    topSellers.appendChild(circle);
+    for(let i=0;i<6;i++){
+        let circle=document.createElement("div");
+        circle.setAttribute("class","sellers");
+        topSellers.appendChild(circle);
+    }
+let product=document.querySelector(".product");
+let spaces=document.querySelectorAll(".space");
+for(let i=0;i<spaces.length;i++) {
+    spaces[i].appendChild(product.cloneNode(true));
 }
-//Product card
-let productsNumber=10;
+let productList=document.querySelectorAll(".product");
+productList[spaces.length].classList.add("finalProduct");
+let buttons=document.querySelectorAll(".firstButton");
+console.log(buttons);
+//Media check
+let cart=document.createElement("i");
 
-for(let i=0;i<6;i++){
-
+window.addEventListener("resize",buttonSet);
+function buttonSet(){
+    
+    if(window.matchMedia("(max-width:860px)").matches) buttons.forEach(index=>index.innerHTML="<i class='fas fa-cart-arrow-down'></i>");
+    else buttons.forEach(index=>index.innerHTML="<i class='fas fa-cart-arrow-down'></i> Add to cart");
 }
+buttonSet();
