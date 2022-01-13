50fa678698c52a6d6476b816d983b8d742d7d8d6
myGithub123# <royandugu99009@gmail.com>
2022-01-13 12:55:13 +0545

    Final edit to cart

--- eCommerce/static/js/cart.js
+++ eCommerce/static/js/cart.js
@@ -1,12 +1,25 @@
-const inputBar=document.querySelector(".displayNumber");
+const inputBar=document.querySelectorAll(".displayNumber");
+const incrementButtonLists=document.querySelectorAll(".increment");
+const decrementButtonLists=document.querySelectorAll(".decrement");
+const arrayTransformationI=Array.from(incrementButtonLists);
+const arrayTransformationD=Array.from(decrementButtonLists);
 let i=0;
-function increment(){
+incrementButtonLists.forEach(index=>{
+    index.addEventListener("click",increment);
+})
+decrementButtonLists.forEach(index=>{
+    index.addEventListener("click",decrement);
+})
+
+function increment(event){
+        i=i=inputBar[arrayTransformationI.indexOf(event.target)].value;
         i++;
-        inputBar.value=i;
+        inputBar[arrayTransformationI.indexOf(event.target)].value=i;
 }
-function decrement(){
-    if(i>0){
+function decrement(event){
+     if(inputBar[arrayTransformationD.indexOf(event.target)].value!=0){
+        i=inputBar[arrayTransformationD.indexOf(event.target)].value;
         i--;
-        inputBar.value=i;
-    }
+        inputBar[arrayTransformationD.indexOf(event.target)].value=i;
+     }
 }
